7b9540412e80d8704709429ac0a4e60657a5c46a
Anant Vikram Singh <anantvikramsr.cs20@rvce.edu.in>
2022-04-14 14:55:08 +0530

    Initial commit

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Main = (props) => {
+    return <Container>Leftside</Container>
+};
+
+const Container=styled.div`
+grid-area: main;
+
+`;
+
+export default Main;
